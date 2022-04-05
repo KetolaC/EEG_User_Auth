18fbb7621dc966fd2ce8db96cfeb3a6cb5fe8a50
KetolaC <78930559+KetolaC@users.noreply.github.com>
2022-04-04 22:49:30 -0400

    Update sectionFeatureData.m

--- Data_Splitting/sectionFeatureData.m
+++ Data_Splitting/sectionFeatureData.m
@@ -1,25 +1,42 @@
 clear;
 clc;
 
-band2sec = {'Alpha'; 'Beta'; 'Delta'; 'Gamma'; 'Theta'};
-for p = 4:12        %change participants here
-    for h = 1:5
-        [trainDat, validDat, testDat] = sectionData(p, char(band2sec(h)));
-        trainDatFilename = ['Features_P', num2str(p), '_', char(band2sec(h)), '_TrainingData.csv'];
-        writetable(trainDat,trainDatFilename);
-        validDatFilename = ['Features_P', num2str(p), '_', char(band2sec(h)), '_ValidationData.csv'];
-        writetable(validDat,validDatFilename);
-        testDatFilename = ['Features_P', num2str(p), '_', char(band2sec(h)), '_TestingData.csv'];
-        writetable(testDat,testDatFilename);
-    end
+band2sec = {''; 'Alpha'; 'Beta'; 'Delta'; 'Gamma'; 'Theta'};
+trainBandTables = {[]; []; []; []; []; []};
+validBandTables = {[]; []; []; []; []; []};
+testBandTables = {[]; []; []; []; []; []};
 
-    [trainDat, validDat, testDat] = sectionData(p, char(band2sec(h)));
-    trainDatFilename = ['Features_P', num2str(p), '_TrainingData.csv'];
-    writetable(trainDat,trainDatFilename);
-    validDatFilename = ['Features_P', num2str(p), '_ValidationData.csv'];
-    writetable(validDat,validDatFilename);
-    testDatFilename = ['Features_P', num2str(p), '_TestingData.csv'];
-    writetable(testDat,testDatFilename);
+for p = 1:12        % Go through all 12 participants
+    disp("Participant " + p)
+    for h = 1:6     % Go through combined and individual band data
+        disp("Band: " + h)
+        [trainDat, validDat, testDat] = sectionData(p, band2sec(h));
+%         trainBandTables{h} = vertcat(trainBandTables{h}, trainDat);
+%         validBandTables{h} = vertcat(validBandTables{h}, validDat);
+%         testBandTables{h} = vertcat(testBandTables{h}, testDat);
+        
+        trainBandTables{h} = [trainBandTables{h}; trainDat];
+        validBandTables{h} = [validBandTables{h}; validDat];
+        testBandTables{h} = [testBandTables{h}; testDat];
+    end
     
-    disp(['Participant ', num2str(p), ' completed'])
 end
+
+trainDatFilename = "TrainingData.csv";
+writetable(trainBandTables{1},trainDatFilename);
+validDatFilename = "ValidationData.csv";
+writetable(validBandTables{1},validDatFilename);
+testDatFilename = "TestingData.csv";
+writetable(testBandTables{1},testDatFilename);
+
+for h = 2:6
+    trainDatFilename =  band2sec(h) + "_TrainingData.csv";
+    writetable(trainBandTables{h},trainDatFilename);
+    validDatFilename = band2sec(h) + "_ValidationData.csv";
+    writetable(validBandTables{h},validDatFilename);
+    testDatFilename = band2sec(h) + "_TestingData.csv";
+    writetable(testBandTables{h},testDatFilename);
+end
+
+disp("Data Splitting Completed")
+
