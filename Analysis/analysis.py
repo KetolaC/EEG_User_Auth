8b8d231bab73c17351c7b51769eeb0a2bc3a4197
KetolaC <78930559+KetolaC@users.noreply.github.com>
2022-08-28 21:01:04 -0400

    Update analysis.py
    Added functions and participant dictionary classes

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -7,6 +7,32 @@
 
 import re
 
+class _ParticipantDictionary(dict):
+
+    def __init__(self, *args, **kwargs):
+        """
+        A class to store participant data
+        :param args: key
+        :param kwargs: value
+        """
+        self.update(*args, **kwargs)    # Add inputs using the dict update function
+
+    def __str__(self):
+        s = ""
+        for k, v in self.items():
+            s = s + '\n' + k + ' : '    # Add newline and key
+            buffer = ' ' * (len(k) + 3)
+
+            for i in v:
+                if i == v[0]:
+                    s = s + str(i) + "\n"
+                else:
+                    s = s + buffer + str(i) + "\n"
+        return s
+
+    def __repr__(self):
+        return "_ParticipantDictionary()"
+
 
 class analysis:
 
@@ -14,88 +40,189 @@ class analysis:
 
         if not isinstance(result_path, str):
             raise ValueError("Result path must be a string containing a valid file path.")
+        if not isinstance(channel_key, list) and channel_key!=None:
+            raise ValueError("Channel key must be a list of strings, integers, or characters.")
 
-        self._path = result_path
-        self._key = channel_key
-        self._num_chn = len(self._key)
-        self._par_data = []
-        self._par = None
-        self._num_par = 0
-        self._readfile()
+        self._path = result_path                        # The filepath to the data
+        self._key = channel_key                         # A key to label channels
+        self._num_chn = 0                               # The number of electrodes used
+        self._par_data = []                             # The scores for every participant
+        self._par = None                                # The label for each participant
+        self._num_par = 0                               # The total number of participants
+        self.__readfile()                               # Read the contents of the provided file
+        self.__setChannelParameters()                   # Determine the number of channels and channel key
 
-    def _readfile(self):
+    def __readfile(self):
+        """
+        A method to extract accuracy and gini importance scores incurred during channel reduction. \n
+        :return: No object is returned
+        """
+
         result_file = open(self._path, 'r')
         result_data = result_file.read()
         self._par = re.findall('Participant.*$', result_data, re.MULTILINE)  # Extracts only scores from text
-        print(self._par)
         self._num_par = len(self._par)
 
         for i in range(self._num_par):
             self._par_data.append([])
 
-        scores = re.findall(r': (.*)', result_data)  # Extracts only scores from text
-
-        ind = -1  # Get an index ready to keep track of participant
+        scores = re.findall(r': (.*)', result_data)     # Extracts only scores from text
+        ind = -1                                        # Get an index ready to keep track of participant
 
         for i in range(len(scores)):
 
-            if i % 3 == 0:  # If benchmark score
-                ind = ind + 1  # Increase index value by 1
-                temp = float(scores[i])  # Convert score from string to decimal
-                self._par_data[ind].append([temp])  # Save score to participant's list first index
-            elif i % 3 == 1:  # If channel rankings
-                self._par_data[ind].append([])  # Save to 2nd index for participant
+            if i % 3 == 0:                              # If benchmark score
+                ind = ind + 1                           # Increase index value by 1
+                temp = float(scores[i])                 # Convert score from string to decimal
+                self._par_data[ind].append([temp])      # Save score to participant's list first index
+            elif i % 3 == 1:                            # If channel rankings
+                self._par_data[ind].append([])          # Save to 2nd index for participant
                 for num in scores[i].split(', '): self._par_data[ind][1].append(float(num))  # Save rankings as decimals
-            else:  # If channel reductions
+            else:                                       # If channel reductions
                 for num in scores[i].split(', '): self._par_data[ind][0].append(
-                    float(num))  # Add as decimals to first index
+                    float(num))                         # Add as decimals to first index
+
+
+    def __setChannelParameters(self):
+        """
+        A method to determine the number of electrodes in use and assign undefined channel keys
+        :return:
+        """
+
+        self._num_chn = len(self._par_data[0][0])
+
+        if self._key == None:
+            self._key = []
+            for n in range(1, self._num_chn + 1):
+                self._key.append('Channel ' + str(n))
+
+    def __getScore(self, gini=0):
+        par_score = list()                              # Create a list to store score
+        for par in self._par_data:                      # Parse through each participant
+            par_score.append(par[gini])                 # Attach score
+        return par_score                                # Return a list of scores for all participants
+
+    def __str__(self):
+        pass
 
     def accuracy(self):
-        for par in self._par_data:
-            return par[0]
+        """
+        Retrieves the accuracy for every participant for every channel reduction.\n
+        :return: A list of lists containing accuracy by participant
+        """
 
-    def average(self):
-        avg = []
+        return self.__getScore()
+
+    def giniImportance(self):
+        """
+      Retrieves the gini importance for every participant for every channel reduction.\n
+      :return: A list of lists containing gini importance by participant
+      """
+
+        return self.__getScore(gini=1)
+
+    def delta(self, scores):
+        all_diff = []
+        diff = []
+        for l in scores:
+            for n in range(len(l) - 1):
+                diff.append(l[n+1]-l[n])
+            all_diff.append(diff)
+        return all_diff
+
+    def average(self, gini=0):
+        """
+        The average function will return the average accuracy or gini importance of each channel. \n
+        :param gini: When gini=1 the average gini importance is calculated. Otherwise, when gini=0 the average accuracy is calculated
+        :return: The average score for each channel
+        """
+
+        avg = []                                        # Create a list to store the average accuracy each reduction
+        for c in range(self._num_chn):                  # Go through all 32 channels
+            avg_acc = 0                                 # Variable to store the average accuracy
+            for i in range(self._num_par):              # Parse through the channel for all participants
+                avg_acc = self._par_data[i][gini][c] + avg_acc  # Tally up the accuracies
+
+            avg_acc = avg_acc / self._num_par           # Get the average accuracy
+            avg.append(avg_acc)                         # Append the average accuracy for the channel to the list
+
+        return avg
+
+    def sortByParticipant(self, *args, gini=0):
+        par_dic = _ParticipantDictionary.fromkeys(self._par, [])    # Create a participant dictionary
+        scores = [self.__getScore(gini=gini)]
+
+        for band in args:
+            scores.append(band.__getScore(gini=gini))
+
+        n = 0
+
+        for p in self._par:
+            par_dic[p] = [ps[n] for ps in scores]
+            n = n + 1
+
+        return par_dic
+
+    def rankChannels(self):
+        top_chn = []
+        temp_avg = self.average(gini=1)
+
         for c in range(self._num_chn):
-            
-
-        # for band in band_data:
-        #     band_avg = []  # Create a list to store the average accuracy each reduction
-        #     for channel in range(32):  # Go through all 32 channels
-        #         avg_acc = 0  # Variable to store the average accuracy
-        #         for i in range(12):  # Parse through the channel for all participants
-        #             avg_acc = band[i][0][channel] + avg_acc  # Tally up the accuracies
-        #
-        #         avg_acc = avg_acc / 12  # Get the average accuracy
-        #         band_avg.append(avg_acc)  # Append the average accuracy for the channel to the list
-        #
-        #     plt.plot(x_axis, band_avg, '-o', label=band_names[band_data.index(band)])  # Plot data
-        #
-        # plt.ylabel("Average Accuracy")  # Label the y-axis
-        # plt.xlabel("Channels Used")  # Label the x-axis
-        # plt.title("Average Accuracy Per Band")  # Add a title
-        # plt.legend(loc=4)  # Add a legend to the plot
-        # fname = "average_accuracy.png"  # Name for the plot savefile
-        # plt.savefig(fname)  # Save the plot
-        # plt.show()  # Create the plot
-        #
-        # del [band, band_avg, channel, avg_acc, i, fname]
-
+            max_gini = temp_avg.index(max(temp_avg))  # Index of highest gini
+            top_chn.append(self._key[max_gini])                              # Store top channel
+            temp_avg[max_gini] = 0.00                                         # Set top channel to 0
+            #
+        return top_chn
 
 if __name__ == "__main__":
+
     import os
 
     cwd = os.getcwd()  # Get the current working directory (cwd)
-    all_data = "../Classification/Results/all_results.txt"  # Name of the file for all bands
 
-    # all_bands = analysis(all_data, 1)                       # Test type identification
-    # all_bands = analysis(all_data, [1, 2, 3])               # Test list validation
+    all_data = "../Classification/Results/all_results.txt"  # Name of the file for all bands
+    theta_data = "../Classification/Results/theta_results.txt"  # Name of the file for theta band
+    delta_data = "../Classification/Results/delta_results.txt"  # Name of the file for delta band
+    alpha_data = "../Classification/Results/alpha_results.txt"  # Name of the file for alpha band
+    beta_data = "../Classification/Results/beta_results.txt"  # Name of the file for beta band
+    gamma_data = "../Classification/Results/gamma_results.txt"  # Name of the file for theta band
+
     placement = ['Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6', 'T7', 'C3', 'Cz', 'C4', 'T8',
                  'TP9', 'CP5', 'CP1', 'CP2', 'CP6', 'TP10', 'P7', 'P3', 'Pz', 'P4', 'P8', 'PO9', 'O1', 'Oz', 'O2',
                  'PO10']  # The position of each channel in order
 
+    test2 = list(range(1, 33))
+
     all_bands = analysis(all_data, placement)  # Test correct inputs
 
-    acc = all_bands.accuracy()
+    theta_band = analysis(theta_data, placement)
+    delta_band = analysis(delta_data, placement)
+    alpha_band = analysis(alpha_data, placement)
+    beta_band = analysis(beta_data, placement)
+    gamma_band = analysis(gamma_data, placement)
 
-    print(acc)
+    sort = all_bands.sortByParticipant(theta_band, delta_band, alpha_band, beta_band, gamma_band, gini=0)
+
+    print(sort)
+
+    # acc = all_bands.accuracy()
+    # print(acc)
+    #
+    # gini = all_bands.giniImportance()
+    # print(gini)
+    #
+    # delta = all_bands.delta(acc)
+    # print(delta)
+    #
+    # print(all_bands._num_chn)
+    # avg = all_bands.average()
+    #
+    # print(avg)
+    #
+    # avg = all_bands.average(gini=1)
+    #
+    # print(avg)
+
+    # ranked = all_bands.rankChannels()
+    #
+    # print(ranked)
